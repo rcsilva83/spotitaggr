70606269c01f5dd187ae760913c2fa8001c81960
Rodrigo Carvalho Silva <rcsilva83@gmail.com>
2020-04-18 21:08:19 +0000

    Adding basic integration to Spotify

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../spotify.service';
+import SpotifyWebApi from 'spotify-web-api-js';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  playlists: SpotifyApi.PlaylistObjectSimplified[];
+  
+  constructor(private spotifyService: SpotifyService) {}
 
-  constructor() {}
+  ngOnInit() {
+    this.spotifyService.getPlaylists().then((data) => {
+      this.playlists = data.items;
+    });
+  }
 
 }
