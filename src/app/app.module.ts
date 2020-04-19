7629d20daea1a0768a543ba0cc653c3d333f4e00
Rodrigo Carvalho Silva <rcsilva83@gmail.com>
2020-04-19 19:04:17 +0000

    moving spotify service to services folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SpotifyService } from './spotify.service';
+import { SpotifyService } from './services/spotify.service';
 
 @NgModule({
   declarations: [AppComponent],
---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,13 @@
+import * as SpotifyWebApi from 'spotify-web-api-js';
+
+export class SpotifyService {
+  private spotifyApi = new SpotifyWebApi.default();
+
+  constructor() {
+    this.spotifyApi.setAccessToken('BQAZY0xzEDpUAq0PdUNYpjDMAiiTIMLlgjpkGzNMZtAXYaJxGoKdlGUI9y69vi1uy1c4uLB9AlxY1o1oCMYUUB12G5d7bEHILaAC4Dy-muoi-R7CbFeXFLvfaE9_27EJ0AylXMOxdg9tpkHryKDOzel0X2JGPitSjkqIaD1p1ysww46coSll-lKkpHtxfCUe8sOq-FSAdPCjfRri_tsYU5G-zIM6VLIS6mtBQYdTLu2K7O5v6tcb_nhsVHap8jzX_FBG8cWEDL8ug_AlOXsb_gCOy07l');
+  }
+
+  getPlaylists() {
+    return this.spotifyApi.getUserPlaylists();
+  }
+}
